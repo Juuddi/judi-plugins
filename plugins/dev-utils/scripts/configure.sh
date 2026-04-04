888f3b7f2ae1c1a0f4da5829dc7eeeb441f24f5c
Juuddi <98721462+Juuddi@users.noreply.github.com>
2026-04-04 16:21:33 -0500

    Fix hook script

--- plugins/dev-utils/scripts/configure.sh
+++ plugins/dev-utils/scripts/configure.sh
@@ -5,6 +5,8 @@ CONFIG_FILE="${CLAUDE_PLUGIN_DATA}/config"
 KEY="$1"
 VALUE="$2"
 
+mkdir -p "${CLAUDE_PLUGIN_DATA}"
+
 # Create or update a key=value pair in the config file.
 if [ -f "$CONFIG_FILE" ] && grep -q "^${KEY}=" "$CONFIG_FILE"; then
   sed -i '' "s|^${KEY}=.*|${KEY}=${VALUE}|" "$CONFIG_FILE"
